61ac01efe5bc263553d2f7f669d88a85a5c71390
rfinn <rfinn>
2023-08-27 11:46:26 -0400

    gui failures

--- python/get_coadds_missing_gui_output.py
+++ python/get_coadds_missing_gui_output.py
@@ -3,13 +3,17 @@
 import os
 from astropy.table import Table
 import glob
+
+print()
+print("Checking for coadds in virgo-coadds-fullpath.txt with no output file")
+print()
 # read in virgo-coadds-fullpath.txt
 infile = open('virgo-coadds-fullpath.txt')
 filenames = []
 for line in infile:
     filenames.append(line.strip())
 
-print(filenames[0])
+#print(filenames[0])
 
 for f in filenames:
     #print(f)
