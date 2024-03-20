7a69b71b242ebf661974ed86bec1f11cf9a1249c
rfinn <rfinn>
2024-03-19 22:56:51 -0400

    updating figure with halpha contours

--- python/ngc5364paper.py
+++ python/ngc5364paper.py
@@ -9,6 +9,25 @@ import sys
 sys.path.append(homedir+'/github/HalphaImaging/python3/')
 import plot_cutouts_ha as pch
 
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from astropy.io import fits
+from astropy.wcs import WCS
+
+from PIL import Image
+
+import glob
+
+homedir = os.getenv("HOME")
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+%matplotlib inline
+mycolors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+plotdir = homedir+'/research/Virgo/plots/halpha/'
 
 def get_legacy_images():
 
@@ -35,3 +54,93 @@ def get_legacy_images():
     fits_name, jpeg_name = pch.get_legacy_images(ra,dec,galid='NGC5364_group',imsize=imsize,band='r',subfolder='legacy',makeplots=True)
     return  fits_name, jpeg_name
     
+def plot_mstar_sfr(dirname,xmin=None,xmax=None,ymin=None,ymax=None,xticks=True,figsize=[16,6],cbfrac=.08,cbaspect=20,clevels=[4,5],contourFlag=False):
+    %matplotlib inline
+    os.chdir(homedir+'/research/Virgo-dev/cont-sub-gr')
+    # add scale factor for continuue after the directory name
+
+
+    cwd = os.getcwd()
+    os.chdir(dirname)
+    massim = dirname+"-logmstar-vr.fits"
+    sfrim = dirname+"-sfr-vr.fits"
+    ssfrim = dirname+"-ssfr.fits"
+    mask = dirname+'-R-mask.fits'
+    titles = ['log Mstar','SFR','log sSFR']
+    vmin = [2,0,-11.5]
+    vmax = [6,1.e-6,-9]
+    allim = [massim,sfrim,ssfrim]
+    
+
+    
+    plt.figure(figsize=(figsize[0],figsize[1]))
+
+    plt.subplots_adjust(wspace=0.1)
+    maskdat = fits.getdata(mask)
+
+    allax = []
+    for i, im in enumerate(allim):
+        plt.subplot(1,4,i+2)
+        dat = fits.getdata(im)
+        mdat = np.ma.array(dat,mask=maskdat)
+        if xmin == None:
+            mdat = mdat
+        else:
+            mdat = mdat[ymin:ymax,xmin:xmax]
+        if i == 2:
+            plt.imshow(mdat,vmin=vmin[i],vmax=vmax[i],origin='lower',interpolation='nearest')
+        else:
+            display_image(mdat,percent=99.5,cmap='viridis')#,vmin=vmin[i],vmax=vmax[i])
+        
+        
+        if not xticks: 
+            plt.xticks([],[])
+            plt.yticks([],[])
+        plt.title(titles[i],fontsize=20)
+        plt.colorbar(fraction=cbfrac,aspect=cbaspect)
+        if i == 1:
+            t = dirname.split('-')
+            plt.xlabel(t[0]+' '+t[1],fontsize=20)
+        # plot contours from mass
+        allax.append(plt.gca())
+    # plot the legacy image in panel 1
+    xcoords = np.array([xmin,xmax])
+    ycoords = np.array([ymin,ymax])
+    
+    # get ramin,ramax and decmin,decmax from SFR image
+    sfrim = dirname+"-sfr-vr.fits"
+    header = fits.getheader(sfrim)
+    wcs = WCS(header)
+    sky = wcs.pixel_to_world(xcoords,ycoords)
+    
+    # read in header from legacy r-band image
+    legacyr = glob.glob("legacy/*r.fits")[0]
+    #print(legacyr)
+    legacy_jpg = legacyr.replace('-r.fits','.jpg')
+    jpeg_data = Image.open(legacy_jpg)
+    legwcs = WCS(fits.getheader(legacyr))
+    # plot jpg as projection of legacy r-band
+    plt.subplot(1,4,1,projection=legwcs)
+    plt.imshow(jpeg_data)
+    # set limits in ra,dec
+    x,y = legwcs.world_to_pixel(sky)
+    # convert ramin,ramax and decmin,decmax to (x,y)
+    print(sky)
+    plt.axis([x[0],x[1],y[0],y[1]])
+    #print(x[0],x[1],y[0],y[1])
+    if contourFlag:
+        # get contours from logmstar image
+        hdu = fits.open(massim)
+        contour_data = hdu[0].data
+        contour_header = hdu[0].header
+        hdu.close()
+        mcontour_data = np.ma.array(contour_data,mask=maskdat)
+        for ax in allax:
+            ax.contour((mcontour_data[ymin:ymax,xmin:xmax]),levels=clevels, colors='k',linestyles='-',linewidths=1)#,transform=ax.get_transform(WCS(contour_header))
+    #plt.contour(contour_data[y[0]:y[1],x[0]:x[1]],levels = [4,5,6],colors='c',alpha=0.5)
+    plt.title("Legacy grz",fontsize=20)
+   
+    plt.savefig(os.path.join(plotdir,dirname)+'-mstar-sfr-ssfr.png')
+
+
+    os.chdir(cwd)
