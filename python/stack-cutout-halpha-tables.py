4013ac77d124630d6f5d42bd03af886c739e2a1f
rfinn <rfinn>
2024-09-29 17:39:10 -0400

    working on analyzing full sample, with updated CS images.
    adding program to stack the results from running photwrapper on the color-based continuum-subtracted images.

---
+++ python/stack-cutout-halpha-tables.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+'''
+GOAL:
+* this is an updated Virgo/programs/stack-gui-halpha-tables.py
+* I am updating to run on the fits files that are in the cutouts/VFID* directories
+* the difference is that these were run AFTER the color-based continuum subtraction with the hand-tuned scale factor,
+so the sequencing is different from just running after running the gui in auto mode.
+
+
+* combine tables that are created from get_gr_cont_phot.py
+* astropy.table vstack is really slow, so I am going to try to do it more manually
+
+DATA:
+* the most recent directory is /data-pool/Halpha/halphagui-output-20240522/cutouts/
+* run from this directory
+
+PROCEDURE:
+* count total lines in input files
+* create a new empty array with the right number of rows and dataype of the individual tables
+* write individual tables into the combined table
+
+
+OUTPUT:
+* combined table
+'''
+
+from datetime import date
+import glob
+import numpy as np
+
+from astropy.io import fits
+from astropy.table import Table
+
+# get input files
+flist = flist1 + flist2 + flist3
+flist = glob.glob("VFID*/halpha-csgr-rfinn*.fits")
+flist.sort()
+print(f"Found {len(flist)} files to stack")
+print()
+# get total number of lines
+print('getting total number of lines')
+nlines=0
+for f in flist:
+    t = fits.getdata(f)
+    nlines += len(t)
+
+# create output table
+print(f"nlines = {nlines}")
+outtab = np.zeros(nlines,dtype=t.dtype)
+
+# loop through input tables and write the rows into the output table
+print('filling in output table')
+startindex=0
+for f in flist:
+    t = fits.getdata(f)
+    n = len(t)
+    outtab[startindex:startindex+n] = t
+    startindex += n
+
+# write the output table
+print('writing output table')
+today = date.today()
+str_date_today = today.strftime('%Y-%b-%d')
+outtab = Table(outtab)
+outtab_name = 'halphagui-output-combined-{}.fits'.format(str_date_today)
+
+outtab.write(outtab_name,overwrite=True)
