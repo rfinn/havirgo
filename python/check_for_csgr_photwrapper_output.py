b6d4142929353bd8fc58a54924aca579fa91059d
rfinn <rfinn>
2024-09-29 17:54:22 -0400

    working on analyzing full sample, with updated CS images.
    adding program to stack the results from running photwrapper on the color-based continuum-subtracted images.

---
+++ python/check_for_csgr_photwrapper_output.py
@@ -0,0 +1,18 @@
+from datetime import date
+import glob
+import numpy as np
+import os
+from astropy.io import fits
+from astropy.table import Table
+
+# get input files
+alldirs = open('virgo-cutouts.txt','r')
+alldirlist = alldirs.readlines()
+
+for d in alldirlist:
+    print(f"{d}/halpha-csgr-rfinn-2024-Sep-29.fits")
+    if os.path.exists(f"{d}/halpha-csgr-rfinn-2024-Sep-29.fits"):
+        continue
+    else:
+        print(f"Missing halpha-csgr file for {d}")
+
