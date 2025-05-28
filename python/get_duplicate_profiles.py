0111964cec26d0750b5815adf06b584bc149bed5
rfinn <rfinn@siena.edu>
2025-05-27 21:32:21 -0400

    plotting profiles of duplicate observations

--- python/get_duplicate_profiles.py
+++ python/get_duplicate_profiles.py
@@ -68,7 +68,7 @@ def make_plots(subdirs,vf):
             plt.fill_between(rphot['sma_arcsec'],y1,y2,label=sd,alpha=alphas[i],color=mycolors[i])
             # also plot line because you can't see the result when the error is small
             # this should fix issue #18 in Virgo github
-            plt.plot(rphot['sma_arcsec'],y0,lss[i],lw=2,color=mycolors[i])
+            plt.plot(rphot['sma_arcsec'],y0,lss[j],lw=2,color=mycolors[i])
 
 
             if j < 2:
@@ -78,7 +78,7 @@ def make_plots(subdirs,vf):
             plt.fill_between(t['sma_arcsec'],sb1,sb2,label=sd,alpha=alphas[i],color=mycolors[i])
             # also plot line because you can't see the result when the error is small
             # this should fix issue #18 in Virgo github
-            plt.plot(t['sma_arcsec'],sb0,lss[i],lw=2,color=mycolors[i])
+            plt.plot(t['sma_arcsec'],sb0,lss[j],lw=2,color=mycolors[i])
 
             
     for i in range(4):
